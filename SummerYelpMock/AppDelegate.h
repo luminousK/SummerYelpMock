80e2ccd6f178bffc2776ce580d1d7c582773934b
cyk22 <407205835@qq.com>
2017-08-26 16:10:39 -0500

    Initial Commit

---
+++ SummerYelpMock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SummerYelpMock
+//
+//  Created by cyk on 2017/8/26.
+//  Copyright © 2017年 cyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SummerYelpMock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SummerYelpMock
+//
+//  Created by cyk on 2017/8/26.
+//  Copyright © 2017年 cyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
